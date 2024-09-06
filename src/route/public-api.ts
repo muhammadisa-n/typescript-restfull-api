4379a43d6437b87558afcc705fdba79d60d928e9
muhammadisa226 <muhammadisa226@gmail.com>
2024-09-06 23:13:13 +0700

    done

--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -1,6 +1,31 @@
-import express from "express";
+import express, { Request, NextFunction, Response } from "express";
 import { UserController } from "../controller/user-controller";
+import { ResponseError } from "../helper/response-error";
 
 export const publicRouter = express.Router();
+publicRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(200)
+    .json({
+      data: {
+        message: "Server It Works",
+        author: "Muhammad Isa",
+        api_version: "1.0.0",
+      },
+    })
+    .end();
+});
+publicRouter.get("/api", (req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(200)
+    .json({
+      data: {
+        message: "Api It Works",
+        author: "Muhammad Isa",
+        api_version: "1.0.0",
+      },
+    })
+    .end();
+});
 publicRouter.post("/api/users", UserController.register);
 publicRouter.post("/api/users/login", UserController.login);
